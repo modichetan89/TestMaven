b72f80a3da3c355e1e010c2904f371ad73db421a
chetan_modi <chetan_modi@THBSGGNLPT-042.thbs.india.com>
2018-04-13 15:30:16 +0530

    adding new method

--- src/main/java/methods/HomePage_M.java
+++ src/main/java/methods/HomePage_M.java
@@ -1,16 +1,27 @@
 package methods;
 
 
+import java.io.FileInputStream;
 import java.io.IOException;
-
+import java.util.Properties;
 import org.openqa.selenium.interactions.Actions;
-
 import pageObjects.HomePageObj;
 import resources.BaseClass;
 
 public class HomePage_M extends BaseClass{
 	
-	HomePageObj lp = new HomePageObj(driver);
+      Properties prop1 = new Properties();
+	   BaseClass testEnv = new BaseClass();
+		HomePageObj lp = new HomePageObj(driver);
+	   
+	   
+public  HomePage_M () throws IOException {
+	   String fileName = "src/main/java/resources/data.properties";
+	   FileInputStream fin=new FileInputStream(fileName);
+	   prop1.load(fin);
+	}
+	
+
 	
 	public boolean LoginPageNavigation() throws IOException, InterruptedException{
 		boolean flag=true;
@@ -32,13 +43,13 @@ public class HomePage_M extends BaseClass{
 			log.info("Entering the username");
 			//WebDriverWait wait = new WebDriverWait(driver, 10);
 			
-			String USERNAME = prop.getProperty("username");
+			String USERNAME = prop1.getProperty("username");
 			//wait.until(ExpectedConditions.visibilityOfElementLocated((By) lp.getUsername()));
 			lp.getUsername().sendKeys(USERNAME);
 			log.info("Continue to next page to enter password");
 			lp.getUsernameContinue().click();
 			log.info("Entering the password");
-			String PASSWORD = prop.getProperty("password");
+			String PASSWORD = prop1.getProperty("password");
 			lp.getPassword().sendKeys(PASSWORD);
 			log.info("Clicking on Submit button");
 			lp.getSubmitButton().click();
@@ -54,9 +65,11 @@ public class HomePage_M extends BaseClass{
 		try{
 			Actions act = new Actions(driver);
 			act.moveToElement(lp.getLogin()).perform();
-			Thread.sleep(5000);
+			Thread.sleep(2000);
 			boolean link = lp.getSignOutLink().isDisplayed();
-				log.info("User is logged in", link);
+			if(link==true){
+				log.info("User is logged in");
+			}
 		}catch(Exception ex){
 			log.error("Logout link not present or User is not logged in");
 			flag = false;
